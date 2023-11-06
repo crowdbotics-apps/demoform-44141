a1c6fb2d999177af3dfcc835397f3047e32fe752
Neelam Dubey <neelamdubey9200@gmail.com>
2023-11-06 12:31:23 +0530

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- screens/welcome/index.js
+++ screens/welcome/index.js
@@ -1,3 +1,4 @@
+import { Pressable } from "react-native";
 import { TextInput } from "react-native";
 import React from "react";
 import { View, ScrollView, SafeAreaView, StyleSheet } from "react-native";
@@ -11,7 +12,7 @@ const WelcomeScreen = () => {
           
         </View>
         
-      <TextInput style={styles.SkqmOmHL} clearTextOnFocus={true} value="Enter Name"></TextInput></ScrollView>
+      <View style={styles.gSUFIpxl}><TextInput style={styles.bKDHXgCg} value="Enter Name" clearTextOnFocus={true}></TextInput></View><Pressable><View style={styles.ukPZJCIC}></View></Pressable></ScrollView>
     </SafeAreaView>;
 };
 
@@ -29,20 +30,35 @@ const styles = StyleSheet.create({
   group: {
     alignItems: "center"
   },
-  SkqmOmHL: {
-    backgroundColor: "#efe8e8",
-    borderColor: "#cccccc",
-    width: 269,
-    height: 47,
-    left: 50,
-    top: 50,
-    position: "absolute",
-    transform: [{
-      rotate: "0deg"
-    }]
-  },
   HfqLNTda: {
     backgroundColor: "#fffcfc"
+  },
+  gSUFIpxl: {
+    height: 60,
+    width: 250,
+    backgroundColor: "#E4E4E4",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    top: 40,
+    left: 44
+  },
+  bKDHXgCg: {
+    backgroundColor: "#ffffff",
+    borderColor: "#cccccc",
+    width: 250,
+    height: 59
+  },
+  ukPZJCIC: {
+    height: 60,
+    width: 140,
+    backgroundColor: "#1fcad1",
+    borderRadius: 0,
+    color: "#777777",
+    position: "absolute",
+    top: 134,
+    left: 90,
+    borderWidth: 2
   }
 });
 export default WelcomeScreen;
